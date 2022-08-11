97a690f228ce074c21fe5c5d2297d685beb57737
ColterLong <ColterLong@protonmail.com>
2022-08-11 11:44:25 -0500

    script.js will communicate with node.js backend using get and post requests via axios. fortunes pulled from mariadb database instead of locally

--- script.js
+++ script.js
@@ -1,19 +1,51 @@
 // Script to pull random fortune from database
 
-let fortunes = ['You will have excellent luck today',
-                'You will meet an unexpected friend today',
-                'You will have no luck today, try tomorrow'];
+
+const addFortune = document.getElementById('addFortune');
+
+let fortunes = '';
+
+function insertFortune(fortune) {
+  return axios.post('http://127.0.0.1:3000', { "fortune" : fortune })
+    .then(function (response) {
+    console.log(response);
+    })
+}
+
+
+function refreshFortunes() {
+  return axios.get('http://127.0.0.1:3000/list', { params: {} })
+  .then(function (response) {
+    console.log(response);
+    fortunes = response.data.results;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+}
+
+
 
 const fortune = document.getElementById('fortune')
 const button = document.getElementById('btn');
 button.addEventListener("click", () => {
     // get random fortune from fortunes
-    fortune.innerText = fortunes[Math.floor(Math.random() * fortunes.length )];
+    fortune.innerText = fortunes[Math.floor(Math.random() * fortunes.length )].fortune;
 })
 
-const addFortune = document.getElementById('addFortune');
 const addButton = document.getElementById('addBtn');
 addButton.addEventListener("click", () =>{
-    if (addFortune.value) fortunes.push(addFortune.value);
-    addFortune.value = '';
+
+  // don't add empty fortunes
+  if (! addFortune.value) return;
+  insertFortune(addFortune.value)
+  .then((result) => refreshFortunes())
+  .then((result2) => {
+    console.log(fortunes[fortunes.length - 1].fortune);
+  });
+  addFortune.value = '';
 })
+
+// fill arr with initial database
+refreshFortunes()
+
